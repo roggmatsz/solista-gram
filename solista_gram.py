2a4f90eb1ca77a9039f05fd06c992f7f20e6469d
Rogger Matamoros <roggmatz@gmail.com>
2022-04-17 04:24:39 +0000

    Created method to save data back to secrets.json. Made logic a wee more robust.

--- solista_gram.py
+++ solista_gram.py
@@ -1,4 +1,5 @@
 import json
+import time
 import webbrowser
 import requests
 
@@ -7,6 +8,15 @@ def get_secrets():
         parsed_secrets = json.load(secrets_file)
     return parsed_secrets
 
+def update_secrets(key, value):
+    with open('secrets.json', 'r+', encoding='utf-8') as secrets_file:
+        parsed_json = json.load(secrets_file)
+        parsed_json.update({ key: value })
+        secrets_file.seek(0)
+        json.dump(parsed_json, secrets_file, indent=4)
+
+    return parsed_json
+
 AUTH_URL = 'https://api.instagram.com/oauth/authorize'
 TOKEN_URL = 'https://api.instagram.com/oauth/access_token'
 
@@ -14,19 +24,15 @@ secret = get_secrets()['secret']
 client_id = get_secrets()['client_id']
 redirect_uri = get_secrets()['redirect_uri']
 account = get_secrets()['real_account']
-auth_url = f'{AUTH_URL}?client_id={client_id}&redirect_uri={redirect_uri}&scope=user_profile,user_media&response_type=code'
 
-
-# Stuck at the part where user needs to interact with browser to get token
-# auth_response = requests.get(auth_url)
-webbrowser.open(auth_url, new=1)
-authorization_code = input('Enter code in URL')
-
-# with open('secrets.json', 'r+', encoding='utf-8') as secrets_file:
-#     parsed_json = json.load(secrets_file)
-#     parsed_json.update({'auth_code': authorization_code})
-#     secrets_file.seek(0)
-#     json.dump(parsed_json, secrets_file, indent=4)
+authorization_code = None
+if 'authorization_code' in get_secrets():
+    authorization_code = get_secrets()['authorization_code']['value']
+else:
+    auth_url = f'{AUTH_URL}?client_id={client_id}&redirect_uri={redirect_uri}&scope=user_profile,user_media&response_type=code'
+    webbrowser.open(auth_url, new=1)
+    authorization_code = input('Enter code in URL')
+    update_secrets('authorization_code', {'date_updated': time.time(), 'value': authorization_code})
 
 params = {
     'client_id': '1024729738207116',
@@ -36,7 +42,6 @@ params = {
     'code': authorization_code
 }
 token_response = requests.post(TOKEN_URL, data=params)
-#token_response = requests.post(url)
 if token_response.status_code != 200:
     print(token_response.text)
 else:
